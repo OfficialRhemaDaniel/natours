ff4cf1a0141b0c454e13d8c81ce1bfeeaff1c44d
Rhema Daniel <rhemadaniel2004.com>
2022-04-04 16:15:45 +0100

    i learnt about enviroment variables

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // app.use(morgan('dev'));
-
-//ROUTE HANDLERS
-
-//ROUTES
-
-//USERS
-
-//USER ROUTES
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-//port
-
-const port = 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`App is running at ${port}...`);
 });
