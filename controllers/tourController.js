1a61c26fbf66479a95b1b1a8a3d233482e25d831
Rhema Daniel <rhemadaniel2004@gmail.com>
2022-05-14 21:56:14 +0100

    I refactored my code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,6 @@
-//?
 const { countDocuments } = require('./../models/tourModel');
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -12,49 +12,13 @@ exports.aliasTopTours = (req, res, next) => {
 //THE GET REQUEST
 exports.GetAllTours = async (req, res) => {
   try {
-    // BUILD QUERY
-    const queryObj = { ...req.query };
-    const excludedField = ['page', 'sort', 'limit', 'fields'];
-    excludedField.forEach((el) => delete queryObj[el]);
-
-    //ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    //SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    //FIELD LIMITING
-    if (req.query.fields) {
-      //I NEED EXPLANATION
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    //PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist!!!');
-    }
     //EXECUTE THE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     //SEND RESPONSE
     res.status(200).json({
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,4 +1,4 @@
-class APIfeatures {
+class APIFeatures {
   constructor(query, queryString) {
     this.query = query;
     this.queryString = queryString;
@@ -6,40 +6,41 @@ class APIfeatures {
 
   filter() {
     const queryObj = { ...this.queryString };
-
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    const excludedField = ['page', 'sort', 'limit', 'fields'];
+    excludedField.forEach((el) => delete queryObj[el]);
 
     //ADVANCED FILTERING
     let queryStr = JSON.stringify(queryObj);
-
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
+    console.log(JSON.parse(queryStr));
     this.query = this.query.find(JSON.parse(queryStr));
+    console.log('filter');
     return this;
   }
 
   sort() {
     if (this.queryString.sort) {
-      console.log(this.queryString.sort);
       const sortBy = this.queryString.sort.split(',').join(' ');
 
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort('-createdAt');
     }
+    console.log('sort');
     return this;
   }
+
   limitFields() {
     if (this.queryString.fields) {
-      //I NEED EXPLANATION
       const fields = this.queryString.fields.split(',').join(' ');
+      console.log(fields);
       this.query = this.query.select(fields);
     } else {
       this.query = this.query.select('-__v');
     }
     return this;
   }
+
   paginate() {
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 100;
@@ -50,4 +51,4 @@ class APIfeatures {
   }
 }
 
-module.exports = APIfeatures;
+module.exports = APIFeatures;
